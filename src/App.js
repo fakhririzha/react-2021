e658f8cbfbfdd2c7c469ffc2371c77a71b03b5a1
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-01-30 17:20:05 +0700

    Check props is exist using PropTypes isRequired

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 const App = () => {
 	return (
 		<div className="container">
-			<Header title={1}></Header>
+			<Header></Header>
 		</div>
 	);
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,12 +9,12 @@ const Header = ({ title }) => {
 };
 
 // Jika tidak ada passing value di App.js, maka props.title akan nge-load default property yang di definisikan dibawah
-Header.defaultProps = {
-	title: "Task Tracker",
-};
+// Header.defaultProps = {
+// 	title: "Task Tracker",
+// };
 
 Header.propTypes = {
-	title: PropTypes.string,
+	title: PropTypes.string.isRequired,
 };
 
 export default Header;
