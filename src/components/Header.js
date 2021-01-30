60212a4689432acebbd030cdb016cb4d4a49a63c
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-01-30 17:14:00 +0700

    Passing props as structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
-const Header = (props) => {
+const Header = ({ title }) => {
 	return (
 		<header>
-			<h1>{props.title}</h1>
+			<h1>{title}</h1>
 		</header>
 	);
 };
+
+// Jika tidak ada passing value di App.js, maka props.title akan nge-load default property yang di definisikan dibawah
+// Header.defaultProps = {
+// 	title: "Task Tracker",
+// };
 
 export default Header;
