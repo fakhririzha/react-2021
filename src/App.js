ab5fc366322b192efcf1ec23f81d0adb728643e7
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-01-30 16:55:39 +0700

    Using js expression in JSX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 function App() {
+	const name = "Fakhri";
+	const x = true;
+
 	return (
-		<div className="App">
+		<div className="container">
 			<h1>Hello from React!</h1>
+			<h2>Hello {x ? name : ", world!"}</h2>
 		</div>
 	);
 }
