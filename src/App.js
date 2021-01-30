e5c9d238141ce3248b53e748c0899303936c4907
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-01-31 01:38:36 +0700

    Toggle a task reminder sync with json-server mock data

--- src/App.js
+++ src/App.js
@@ -24,6 +24,14 @@ const App = () => {
 		return data;
 	};
 
+	// Fetch Task
+	const fetchTask = async (id) => {
+		const res = await fetch(`http://localhost:5000/tasks/${id}`);
+		const data = await res.json();
+
+		return data;
+	};
+
 	// Add Task
 	const addTask = async (task) => {
 		// const id = Math.floor(Math.random() * 10000) + 1;
@@ -54,10 +62,23 @@ const App = () => {
 	};
 
 	// Toggle Reminder
-	const toggleReminder = (id) => {
+	const toggleReminder = async (id) => {
+		const taskToToggle = await fetchTask(id);
+		const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+		const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(updatedTask),
+		});
+
+		const data = await res.json();
+
 		setTasks(
 			tasks.map((task) =>
-				task.id === id ? { ...task, reminder: !task.reminder } : task
+				task.id === id ? { ...task, reminder: data.reminder } : task
 			)
 		);
 	};
