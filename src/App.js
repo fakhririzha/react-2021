c93a0af2ea7cf0f2af72a91a86eca7410a1c908b
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-01-31 01:30:56 +0700

    Add a task to json-server mock data

--- src/App.js
+++ src/App.js
@@ -25,11 +25,23 @@ const App = () => {
 	};
 
 	// Add Task
-	const addTask = (task) => {
-		const id = Math.floor(Math.random() * 10000) + 1;
+	const addTask = async (task) => {
+		// const id = Math.floor(Math.random() * 10000) + 1;
 
-		const newTask = { id, ...task };
-		setTasks([...tasks, newTask]);
+		// const newTask = { id, ...task };
+		// setTasks([...tasks, newTask]);
+
+		const res = await fetch("http://localhost:5000/tasks", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(task),
+		});
+
+		const data = await res.json();
+
+		setTasks([...tasks, data]);
 	};
 
 	// Delete Task
