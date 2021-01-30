183c76e3dda63e4ccdd04d5d0ab751d094a52af9
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-01-30 18:50:00 +0700

    Using useState to pass a data to Tasks component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,25 +1,27 @@
-const tasks = [
-	{
-		id: 1,
-		text: "Tugas Besar EDS",
-		day: "Jan 31st at 10.00am",
-		reminder: true,
-	},
-	{
-		id: 2,
-		text: "Pilgub Fasilkom-TI",
-		day: "Mar 13th at 14.00pm",
-		reminder: true,
-	},
-	{
-		id: 3,
-		text: "Kuliah Semester Genap",
-		day: "Feb 15th at 08.00am",
-		reminder: false,
-	},
-];
+import { useState } from "react";
 
 const Tasks = () => {
+	const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			text: "Tugas Besar EDS",
+			day: "Jan 31st at 10.00am",
+			reminder: true,
+		},
+		{
+			id: 2,
+			text: "Pilgub Fasilkom-TI",
+			day: "Mar 13th at 14.00pm",
+			reminder: true,
+		},
+		{
+			id: 3,
+			text: "Kuliah Semester Genap",
+			day: "Feb 15th at 08.00am",
+			reminder: false,
+		},
+	]);
+
 	return (
 		<>
 			{tasks.map((task) => (
