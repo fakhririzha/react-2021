0adcd46f137852cfc34788dfae044bc5d30343bd
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-01-30 18:17:03 +0700

    Using handler on button (example)

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,28 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Button = () => {
-	return <button className="btn">Add</button>;
+const Button = ({ color, text }) => {
+	const onClickHandler = () => {
+		console.log("Clicked");
+	};
+	return (
+		<button
+			onClick={onClickHandler}
+			style={{ backgroundColor: color }}
+			className="btn"
+		>
+			{text}
+		</button>
+	);
+};
+
+Button.defaultProps = {
+	color: "steelblue",
+};
+
+Button.propTypes = {
+	text: PropTypes.string.isRequired,
+	color: PropTypes.string.isRequired,
 };
 
 export default Button;
