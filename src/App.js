3301d83e5ce82d154246dfba5e969f6f3783e232
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-01-31 01:54:45 +0700

    Creating Footer and About Page using React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import About from "./components/About";
 
 const App = () => {
 	const [showAddTask, setShowAddTask] = useState(false);
@@ -84,22 +87,34 @@ const App = () => {
 	};
 
 	return (
-		<div className="container">
-			<Header
-				onAdd={() => setShowAddTask(!showAddTask)}
-				showAdd={showAddTask}
-			></Header>
-			{showAddTask && <AddTask onAdd={addTask}></AddTask>}
-			{tasks.length > 0 ? (
-				<Tasks
-					tasks={tasks}
-					onDelete={deleteTask}
-					onToggle={toggleReminder}
-				></Tasks>
-			) : (
-				"No Task To Show"
-			)}
-		</div>
+		<Router>
+			<div className="container">
+				<Header
+					onAdd={() => setShowAddTask(!showAddTask)}
+					showAdd={showAddTask}
+				></Header>
+				<Route
+					path="/"
+					exact
+					render={(props) => (
+						<>
+							{showAddTask && <AddTask onAdd={addTask}></AddTask>}
+							{tasks.length > 0 ? (
+								<Tasks
+									tasks={tasks}
+									onDelete={deleteTask}
+									onToggle={toggleReminder}
+								></Tasks>
+							) : (
+								"No Task To Show"
+							)}
+						</>
+					)}
+				></Route>
+				<Route path="/about" component={About}></Route>
+				<Footer></Footer>
+			</div>
+		</Router>
 	);
 };
 
