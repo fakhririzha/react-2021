4de47b0ad32b9578845745767ca1749857670f72
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-01-30 23:58:11 +0700

    AddTask form complete (next: creating the handler)

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -9,15 +9,29 @@ const AddTask = () => {
 		<form className="add-form">
 			<div className="form-control">
 				<label>Task</label>
-				<input type="text" placeholder="Add Task" value={text}></input>
+				<input
+					type="text"
+					placeholder="Add Task"
+					value={text}
+					onChange={(e) => setText(e.target.value)}
+				></input>
 			</div>
 			<div className="form-control">
 				<label>Day & Time</label>
-				<input type="text" placeholder="Add Day & Time"></input>
+				<input
+					type="text"
+					placeholder="Add Day & Time"
+					value={day}
+					onChange={(e) => setDay(e.target.value)}
+				></input>
 			</div>
 			<div className="form-control form-control-check">
-				<label>Reminder</label>
-				<input type="checkbox"></input>
+				<label>Set Reminder</label>
+				<input
+					type="checkbox"
+					value={reminder}
+					onChange={(e) => setReminder(e.currentTarget.checked)}
+				></input>
 			</div>
 
 			<input type="submit" value="Save Task" className="btn btn-block"></input>
