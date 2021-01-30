3c4f74a3b1042b910f0d2c8fdb29e70fb74f9248
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-01-30 17:44:33 +0700

    Inline css styling in JSX

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,17 @@ import PropTypes from "prop-types";
 const Header = ({ title }) => {
 	return (
 		<header>
-			<h1>{title}</h1>
+			<h1 style={{ color: "tomato", backgroundColor: "darkslategray" }}>
+				{title}
+			</h1>
 		</header>
 	);
 };
 
 // Jika tidak ada passing value di App.js, maka props.title akan nge-load default property yang di definisikan dibawah
-// Header.defaultProps = {
-// 	title: "Task Tracker",
-// };
+Header.defaultProps = {
+	title: "Task Tracker",
+};
 
 Header.propTypes = {
 	title: PropTypes.string.isRequired,
